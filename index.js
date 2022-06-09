95909ee1d94c945b97ddaa9624da5ec3eca3da36
sayman <mdrabbi.asm@gmail.com>
2022-06-09 20:45:20 +0600

    heroku

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 require('dotenv').config()
+//done
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0rbfs.mongodb.net/?retryWrites=true&w=majority`;
